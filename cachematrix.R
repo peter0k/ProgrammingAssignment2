94f8991e3c670e92f3c0c61d1011b0d999333e2a
peter0kiraly <peter0kiraly@gmail.com>
2018-01-12 21:20:38 +0100

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Matrix inversion is usually a costly computation and
+#there may be some benefit to caching the inverse of
+#a matrix rather than computing it repeatedly.
 
-## Write a short comment describing this function
+##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve's argument is the function list returned by
+## makeCacheMatrix. If the inverse has already been calculated
+## (and the matrix has not changed), then cachesolve returns
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## enter cache, return inv if m is not NULL
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+## testing
+install.packages("microbenchmark")
+library(microbenchmark)
+
+X = matrix(c(2,2,3,2),2,2)
+x <- makeCacheMatrix(X)
+cacheSolve(x)
+solve(X)
+
+microbenchmark(
+  cacheSolve(x),
+  solve(X)
+)
+
